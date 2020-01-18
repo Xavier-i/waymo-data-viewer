ee73cee0c66d3b7265ee2fc131d7a3facdc9a8d3
xavier <haohan.li.97@gmail.com>
2020-01-17 18:32:46 -0800

    Add utils

---
+++ utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+import tensorflow as tf
+from waymo_open_dataset import dataset_pb2
+
+def convert_range_image_to_point_cloud_with_intensity(frame,
+                                       range_images,
+                                       camera_projections,
+                                       range_image_top_pose,
+                                       ri_index=0):
+  """Convert range images to point cloud.
+
+  Args:
+    frame: open dataset frame
+     range_images: A dict of {laser_name, [range_image_first_return,
+       range_image_second_return]}.
+     camera_projections: A dict of {laser_name,
+       [camera_projection_from_first_return,
+       camera_projection_from_second_return]}.
+    range_image_top_pose: range image pixel pose for top lidar.
+    ri_index: 0 for the first return, 1 for the second return.
+
+  Returns:
+    points: {[N, 3]} list of 3d lidar points of length 5 (number of lidars).
+    cp_points: {[N, 6]} list of camera projections of length 5
+      (number of lidars).
+  """
+  calibrations = sorted(frame.context.laser_calibrations, key=lambda c: c.name)
+  points = []
+  cp_points = []
+
+  frame_pose = tf.convert_to_tensor(
+      value=np.reshape(np.array(frame.pose.transform), [4, 4]))
+  # [H, W, 6]
+  range_image_top_pose_tensor = tf.reshape(
+      tf.convert_to_tensor(value=range_image_top_pose.data),
+      range_image_top_pose.shape.dims)
+  # [H, W, 3, 3]
+  range_image_top_pose_tensor_rotation = transform_utils.get_rotation_matrix(
+      range_image_top_pose_tensor[..., 0], range_image_top_pose_tensor[..., 1],
+      range_image_top_pose_tensor[..., 2])
+  range_image_top_pose_tensor_translation = range_image_top_pose_tensor[..., 3:]
+  range_image_top_pose_tensor = transform_utils.get_transform(
+      range_image_top_pose_tensor_rotation,
+      range_image_top_pose_tensor_translation)
+  for c in calibrations:
+    range_image = range_images[c.name][ri_index]
+    if len(c.beam_inclinations) == 0:  # pylint: disable=g-explicit-length-test
+      beam_inclinations = range_image_utils.compute_inclination(
+          tf.constant([c.beam_inclination_min, c.beam_inclination_max]),
+          height=range_image.shape.dims[0])
+    else:
+      beam_inclinations = tf.constant(c.beam_inclinations)
+
+    beam_inclinations = tf.reverse(beam_inclinations, axis=[-1])
+    extrinsic = np.reshape(np.array(c.extrinsic.transform), [4, 4])
+
+    range_image_tensor = tf.reshape(
+        tf.convert_to_tensor(value=range_image.data), range_image.shape.dims)
+    pixel_pose_local = None
+    frame_pose_local = None
+    if c.name == dataset_pb2.LaserName.TOP:
+      pixel_pose_local = range_image_top_pose_tensor
+      pixel_pose_local = tf.expand_dims(pixel_pose_local, axis=0)
+      frame_pose_local = tf.expand_dims(frame_pose, axis=0)
+    range_image_mask = range_image_tensor[..., 0] > 0
+    range_image_cartesian = range_image_utils.extract_point_cloud_from_range_image(
+        tf.expand_dims(range_image_tensor[..., 0], axis=0),
+        tf.expand_dims(extrinsic, axis=0),
+        tf.expand_dims(tf.convert_to_tensor(value=beam_inclinations), axis=0),
+        pixel_pose=pixel_pose_local,
+        frame_pose=frame_pose_local)
+
+    range_image_cartesian = tf.squeeze(range_image_cartesian, axis=0)
+    # Append Intensity
+    range_image_inten=range_image_tensor[..., 1].numpy().reshape([range_image.shape.dims[0], range_image.shape.dims[1],1])
+
+    points_real=np.append(range_image_cartesian.numpy(),range_image_inten,axis = 2)
+    
+    points_tensor = tf.gather_nd(points_real,
+                                 tf.compat.v1.where(range_image_mask))
+
+    cp = camera_projections[c.name][0]
+    cp_tensor = tf.reshape(tf.convert_to_tensor(value=cp.data), cp.shape.dims)
+    cp_points_tensor = tf.gather_nd(cp_tensor,
+                                    tf.compat.v1.where(range_image_mask))
+    points.append(points_tensor.numpy())
+    cp_points.append(cp_points_tensor.numpy())
+  return points, cp_points
